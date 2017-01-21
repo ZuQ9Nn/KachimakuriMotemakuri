1dd1848cb5cdf895bdff1d0f9f1afee8f638a5c1
ZuQ9Nn <ZuQ9Nn@gmail.com>
2017-01-21 20:08:27 +0900

    first commit

---
+++ Assets/MyProduct/Scripts/MonyCreator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+using HoloToolkit.Unity.InputModule;
+
+public class MonyCreator : MonoBehaviour, IInputClickHandler
+{
+    [SerializeField]
+    private int MaxCount = 100;
+
+    [SerializeField]
+    private GameObject moneny = null;
+
+    // Use this for initialization
+    private void Start()
+    {
+        InputManager.Instance.PushFallbackInputHandler(gameObject);
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (moneny)
+        {
+            for(int i = 0; i < MaxCount; i++)
+            {
+                Instantiate(moneny, GazeManager.Instance.HitPosition, Quaternion.identity);
+            }
+        }
+    }
+}
